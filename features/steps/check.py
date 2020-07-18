12242b4c682d1f63bf90326447bcdbf4e2254fcf
Student-01-Jason <63417122+Student-01-Jason@users.noreply.github.com>
2020-07-19 00:46:12 +0100

    Update check.py

--- features/steps/check.py
+++ features/steps/check.py
@@ -1,6 +1,14 @@
 from behave import *
+
 from gamecode import game
 
 @Given('I have a string {word}')
 def step_impl(context, word):
-#TODO
+    context.string = word
+
+
+@When('the bowling game starts')
+def step_impl(context):
+    pass
+
+
