edcd274049dd7408daefe79c14750f5b85d88975
Student-01-Jason <63417122+Student-01-Jason@users.noreply.github.com>
2020-07-20 11:14:09 +0100

    Update check.py

--- features/steps/check.py
+++ features/steps/check.py
@@ -2,17 +2,17 @@ from behave import *
 
 from gamecode import game
 
-@Given('I have a string {word}')
-def step_impl(context, word):
-    context.string = word
+ @Given('I have a string {word}')
+ def step_impl(context, word):
+   context.word=word
 
 
-@When('the bowling game starts')
-def step_impl(context):
-    pass
+ @When('the bowling game starts')
+ def step_impl(context):
+     pass
 
 
-@Then('I get {score:d} in the game')
-def step_impl(context, score):
+ @Then('I get {score:d} in the game')
+ def step_impl(context, score):
     s = game.get_score(context.string)
-    assert s == score
+     assert s == score
